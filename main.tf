4d054f710789c3f90b32dc7df0a2aee9af4c815a
fiend2 <88592069+fiend2@users.noreply.github.com>
2021-10-19 17:28:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
 provider "aws" {
-   region     = var.region
-   access_key = var.access_key
-   secret_key = var.secret_key    
-   
+   region     = var.region   
 }
 
 resource "aws_instance" "ec2_example" {
