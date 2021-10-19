bf7e1b34b944a20218b33fa12173e6a0fad8fc79
Fiend <fiend@gmail.com>
2021-10-19 16:34:01 +0530

    Added

--- var.tf
+++ var.tf
@@ -11,12 +11,13 @@ variable "instance_type" {
     default = "t2.micro"
   
 }
-
+/*
 variable "ssh_key" {
     type = string
     default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDYmsdm2eQW5YogpZg3YdNbQwUSjRYO5rXl6qIDeW5TNNutO/30EUruzOjdz4iceEIiMAJvDK3vf0A1XKySTN6oFZu/f43rjJ9Wunppc7EGnJoVs+lz0qVBHU8uGg904avWSlMSQp3tlT4znxMSFHFTKm54EpYOVX3G/tEw9+6l2CUKibVW0rDOXH1aoVZNaa+CSpTFSzxih9Svmrc+YYQi/qBht/jYf5DUPwZc2vfFXa2CbcC8MOI8Toia4jyv8HJ9SrIZiBYl/tZz9LdWQIl1axl4L+aQdfmQHphTXEjQjdI38j/KcY1bcDF4HaZshvRs4iKQe8ExXvuhElQWoFU1"
   
 }
+*/
 variable "vpc_id" {
     type = string
     default = "vpc-6c929914"
