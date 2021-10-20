51d76c3624acc235e89c30249a784f886a6b1d38
fiend2 <88592069+fiend2@users.noreply.github.com>
2021-10-20 12:38:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,38 @@
 provider "aws" {
-   region     = var.region   
+  region = "us-west-2"
 }
+
 locals {
-	 ingress_rules = [
-	{
-	    port = 22
-	},
-	{
-	    port = 80
-	},
-	{
-	    port = 81
-	}
+         ingress_rules = [
+        {
+            port = 22
+        },
+        {
+            port = 80
+        },
+        {
+            port = 81
+        }
   ]
 }
 resource "aws_security_group" "sg" {
   name        = "sg"
   description = "Allow TLS inbound traffic"
-  vpc_id      = var.vpc_id
-  
-	
-	dynamic "ingress" {
-	    for_each     = local.ingress_rules
-	  
-	  content {
-	    from_port  = ingress.value.port
-		 to_port     = ingress.value.port
-		 protocol    = "tcp"
-		 cidr_blocks = ["0.0.0.0/0"]
-	   }
-	}
+  vpc_id      = "vpc-6c929914"
+
+
+        dynamic "ingress" {
+            for_each     = local.ingress_rules
+
+          content {
+            from_port  = ingress.value.port
+                 to_port     = ingress.value.port
+                 protocol    = "tcp"
+                 cidr_blocks = ["0.0.0.0/0"]
+           }
+        }
         tags = {
         Name = "sg"
-  		}
 }
+}
+
