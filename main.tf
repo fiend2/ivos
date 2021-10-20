f449f183a94753df4cfcbc2b486e8babd1663f2e
fiend2 <88592069+fiend2@users.noreply.github.com>
2021-10-20 12:37:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,51 +1,6 @@
 provider "aws" {
-   access_key = file("/opt/access_key")
-   secret_key = file("/opt/secret_key")
    region     = var.region   
 }
-
-resource "aws_instance" "ec2_example" {
-
-    ami                    = var.ami 
-    instance_type          = var.instance_type 
-   // key_name               = "aws_key"
-   // vpc_security_group_ids = [aws_security_group.sg.id]
-/*
-     connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = "ubuntu"
-      private_key = file("C:/Users/Noob/Downloads/transit_test.pem")
-   }
-
-  provisioner "file" {
-    content      = "ip-address is: ${self.public_ip}"
-    destination = "/home/ubuntu/ip.log"
-  }
-
-  provisioner "file" {
-    source      = "new_user_credentials.csv"
-    destination = "/home/ubuntu/new_user_credentials.csv"
-  }
- */
-}
-/*
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id            = var.vpc_id
-  service_name      = "com.amazonaws.s3-global.accesspoint"
-  vpc_endpoint_type = "Interface"
-
-  security_group_ids = [aws_security_group.sg.id]
-
-  private_dns_enabled = true
-}
-
-resource "aws_s3_bucket_object" "test-0258_object" {
-  key    = "new_user_credentials.csv"
-  bucket = var.aws_s3_bucket_object
-  source = "new_user_credentials.csv"
-}
-*/
 locals {
 	 ingress_rules = [
 	{
@@ -79,10 +34,3 @@ resource "aws_security_group" "sg" {
         Name = "sg"
   		}
 }
-/*
-resource "aws_key_pair" "deployer" {
-  key_name   = "aws_key"
-  public_key = var.ssh_key
-}
-
-*/
