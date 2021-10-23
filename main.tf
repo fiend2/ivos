6d148cb9a032bec5a4fa4caa36bc9317531b8f00
fiend2 <88592069+fiend2@users.noreply.github.com>
2021-10-23 22:40:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,6 +35,24 @@ resource "aws_security_group" "sg" {
         Name = "sg"
 }
 }
+
+resource "aws_instance" "testing" {
+    ami           = "ami-0df99b3a8349462c6"
+    instance_type = "t2.micro"
+    key_name = "transit_test.pem"
+    user_data = <<-EOF
+                #!/bin/bash
+                apt install httpd -y
+                echo "My first" > /var/www/html/index.html
+                apt update -y
+                service httpd start
+                EOF
+    tags = {
+        Name = test
+    }
+}
+
+
 output "owner_id_and_arn" {
   value = "owner_id = ${aws_security_group.sg.owner_id} and arn = ${aws_security_group.sg.arn}"
 }
